9b0791739f655566cd819956061ceb83218ccef1
Fadumina <bafadumi@amazon.com>
2024-03-21 15:24:25 +0000

    Refactoring

---
+++ cdk/lib/devop_uni-stack.ts
@@ -0,0 +1,108 @@
+import * as cdk from 'aws-cdk-lib';
+import { Stack, StackProps, Stage as CDKStage, StageProps } from 'aws-cdk-lib';
+import { Construct } from 'constructs';
+import { CodeBuildStep, CodePipeline, CodePipelineSource, ShellStep } from 'aws-cdk-lib/pipelines';
+import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { STAGES, ENV, CONFIG } from "./config";
+import { AuthStack } from './stacks/auth-stack';
+import { BackendStack } from './stacks/backend-stack';
+import { CustomStage } from './types';
+import { UIStack } from './stacks/front-end-stack';
+import * as codepipeline from '@aws-cdk/aws-codepipeline';
+import * as codepipeline_actions from '@aws-cdk/aws-codepipeline-actions';
+
+const GITHUB_SOURCE_REPO = 'bafadumi/DevopUni';
+
+const HOSTED_ZONE_ID = 'Z010502913W53UOD7EHXA';
+const HOSTED_ZONE_NAME = 'devops.com';
+
+const DOMAIN_SUFFIX = `learn-be-curious.${HOSTED_ZONE_NAME}`;
+const STEP_COMMANDS = ['npm ci', 'npm run bootstrap', 'npm run install-all', 'npm run build'];
+const BETA_DOMAIN_NAME = `beta.${DOMAIN_SUFFIX}`;
+const PROD_DOMAIN_NAME = `${DOMAIN_SUFFIX}`;
+
+interface CustomStageProps extends StageProps {
+  stage: CustomStage;
+  hostedZoneId: string;
+  hostedZoneName: string;
+  domainName: string;
+}
+
+const BACKEND_ASSET_ROUTE = '../DevopUni/backend';
+const FRONTEND_ASSET_ROUTE = '../DevopUni/frontend/build';
+
+export class PipelineStage extends CDKStage {
+  constructor(scope: Construct, id: string, props: CustomStageProps) {
+    super(scope, id, props);
+
+    const { stage, hostedZoneName, domainName, hostedZoneId } = props;
+
+    const { userPool, userPoolClient } = new AuthStack(this, `${stage}DevOpsAuthStack`, { stage: stage });
+
+    new BackendStack(this, `${stage}DevOpsBackend`, {
+      userPool,
+      assetRoute: BACKEND_ASSET_ROUTE,
+      userPoolClient,
+      stage,
+    });
+
+    new UIStack(this, `${stage}DevOpsUIStack`, {
+      domainName,
+      hostedZoneName,
+      stage,
+      hostedZoneId,
+      frontEndAssetRoute: FRONTEND_ASSET_ROUTE,
+    });
+  }
+}
+
+export class DevopUniStack extends cdk.Stack {
+  private pipeline: CodePipeline;
+
+  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+    super(scope, id, props);
+
+    const sourceAction = CodePipelineSource.gitHub('bafadumi/DevopUni', 'main', {
+      authentication: cdk.SecretValue.secretsManager('my-secret-token'),
+    });;
+
+    const frontendBuildAction = new CodeBuildStep('FrontendBuild', {
+      input: sourceAction,
+      commands: ['cd frontend', 'npm ci', 'npm run build', 'cd ..'],
+      primaryOutputDirectory: './',
+      env: {
+        AWS_GITHUB_TOKEN: cdk.SecretValue.secretsManager('my-secret-token').unsafeUnwrap(),
+      },
+    });
+
+    const backendBuildAction = new CodeBuildStep('BackendBuild', {
+      input: frontendBuildAction,
+      commands: ['cd backend', 'npm ci', 'npm run build', 'cd ..'],
+      primaryOutputDirectory: './',
+      env: {
+        AWS_GITHUB_TOKEN: cdk.SecretValue.secretsManager('my-secret-token').unsafeUnwrap(),
+      },
+
+    });
+
+    const synthAction = new CodeBuildStep('Synth', {
+      input: backendBuildAction,
+      installCommands: [
+        // Globally install cdk in the container
+        'npm install -g aws-cdk',
+      ],
+      commands: ['cd bin', 'npm ci', 'npm run build', 'npx cdk synth', 'cd ..'],
+      // Synth step must output to cdk.out for mutation/deployment
+      primaryOutputDirectory: './',
+      env: {
+        AWS_GITHUB_TOKEN: cdk.SecretValue.secretsManager('my-secret-token').unsafeUnwrap(),
+      },
+    });
+
+    new CodePipeline(this, 'Pipeline', {
+      pipelineName: 'DevOpsAssignmentPipeline',
+      synth: synthAction,
+
+    });
+  }
+}
