22b2fc8c6fae80f7f285248407a18a1401780e74
Fadumina <bafadumi@amazon.com>
2024-03-22 13:18:17 +0000

    Adding correct userpool

--- frontend/src/config/cognitoConfig.ts
+++ frontend/src/config/cognitoConfig.ts
@@ -1,6 +1,6 @@
 import { ICognitoUserPoolData } from 'amazon-cognito-identity-js';
 
 export const COGNITO_CONFIG: ICognitoUserPoolData = {
-    UserPoolId: 'us-east-1_0zfLPegHF',
-    ClientId: '7ns9oaucbi7niefddnqfb50mnr',
+    UserPoolId: 'us-east-1_8TKHal342',
+    ClientId: '43basndn68ahant02kceb1tifv',
 };
